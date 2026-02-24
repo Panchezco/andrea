f00726ba681887a3abd33a9e34ef43871d8e8628
Jorge Lozano <167137050+Panchezco@users.noreply.github.com>
2026-02-24 11:08:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const btnSi = document.getElementById('btn-si');
 const btnNo = document.getElementById('btn-no');
-
 let siScale = 1;
 
+btnSi.addEventListener('click', () => {
+  document.body.innerHTML = `
+    <div style="text-align:center; font-family:Arial; margin-top:10vh;">
+      <img src="lm.jpeg" style="width:300px; border-radius:20px;">
+      <h1 style="color:#ff4d8d;">😉</h1>
+    </div>
+  `;
+});
+
 btnNo.addEventListener('click', () => {
-  // El botón Sí crece
   siScale += 0.3;
   btnSi.style.transform = `scale(${siScale})`;
-
-  // El botón No se mueve a un lugar aleatorio
   const margen = 80;
   const randX = Math.random() * (window.innerWidth - margen * 2) + margen;
   const randY = Math.random() * (window.innerHeight - margen * 2) + margen;
-
   btnNo.style.position = 'fixed';
   btnNo.style.left = randX + 'px';
   btnNo.style.top = randY + 'px';
